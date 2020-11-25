103d934fdde47f06b8e34ebd24d9a32272998533
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-25 18:39:14 -0500

    added formatResp

--- better-weather/src/App.js
+++ better-weather/src/App.js
@@ -1,8 +1,31 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// import { getLocation } from "./utils/getLocation";
+import { formatResp } from "./utils/formatResp";
 
 import Search from "./components/Search";
+import { API_KEY, BASE_URL } from "./constants/index";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    axios
+      .get(`${BASE_URL}/forecast.json?key=${API_KEY}&q=07112&days=10`)
+      .then((res) => {
+        setData(formatResp(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
---
+++ better-weather/src/utils/getLocation.js
@@ -0,0 +1,15 @@
+export const getLocation = async () => {
+  // if (navigator.geolocation) {
+  //   const location = await navigator.geolocation.getCurrentPosition(
+  //     const loc = {};
+  //     (position) => {
+  //       loc["longitude"] = position.coords.longitude;
+  //       loc["latitude"] = position.coords.latitude;
+  //       return loc;
+  //     }
+  //   );
+  //   return location;
+  // } else {
+  //   return "Geolocation is unavailable";
+  // }
+};
